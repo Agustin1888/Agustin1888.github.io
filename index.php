08c49b2b7eeb9778ac680adb87645902c8c2e5de
Agustin1888 <joseagustinsanchezpacheco@gmail.com>
2025-04-06 21:43:01 +0200

    Cambio_php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>El fornet</title>
+    <link rel="stylesheet" href="Tarea_final_CSS.css">
+</head>
+<body>
+    <header>
+        <h1>Bienvenido a "El fornet"</h1>
+        <nav>
+            <ul>
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="productos.html">Productos</a></li>
+                <li><a href="Contactos.html">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <main>
+        <section>
+            <h2>Nuestros Productos</h2>
+            <p>Explora nuestra variedad de productos y servicios.</p>
+        </section>
+    </main>
+    
+    <footer>
+        <p>&copy; 2025 Mi Sitio Web. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
